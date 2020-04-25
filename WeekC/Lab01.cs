3853c2b7ff05db5e8a6a88ec44308ad21dfa9177
pdspatrick <pdspatrick@gmail.com>
2020-04-24 22:41:09 -0500

    added week c

---
+++ WeekC/Lab01.cs
@@ -0,0 +1,55 @@
+﻿using System;
+public class Lab01
+{
+    public static void Main(string[] args)
+    {
+        //
+        Console.WriteLine("CS 201 Restaurant Guide\n");
+        string response;
+        char s, f;
+        bool spicy, fancy;
+        // Ask user for his/her preference
+        Console.Write("Do you like spicy food? (y / n) : ");
+        response = Console.ReadLine();
+        s = response[0];
+        if (s == 'y' || s == 'Y')
+        {
+            spicy = true;
+        }
+        else
+        {
+            spicy = false;
+        }
+        // Ask user for his/her preference
+        Console.Write("Do you want to go to a fancy restaurant? (y / n) : ");
+        // Get the next token
+        response = Console.ReadLine();
+        // Look only at first character
+        f = response[0];
+        fancy = (f == 'y') || (f == 'Y');
+        // Make suggestion
+        if (spicy)
+        {
+            if (fancy)
+            {
+                Console.WriteLine("I suggest you go to Thai Garden Palace.");
+            }
+            else
+            {
+                Console.WriteLine("I suggest you go to Alberto's Tacqueria.");
+            }
+        }
+        else
+        {
+            if (!fancy)
+            {
+                Console.WriteLine("I suggest you go to Joe's Diner.");
+            }
+            else
+            {
+                Console.WriteLine("I suggest you go to Chez Paris.");
+            }
+        }
+        return;
+    }
+}
---
+++ WeekC/Lab02.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace it483
+{
+    class Lab02
+    {
+        public static void Main(string[] args)
+        {
+            string rx = "0";
+            string tint = "0";
+            double total = 0.0;
+            Console.WriteLine("What kind of glasses would you like?");
+            while ((rx != "1") && (rx != "2"))
+            {
+                Console.WriteLine("1 -> prescription, 2 -> non-prescription");
+                string rxin = Console.ReadLine();
+                rx = rxin;
+            }
+            while ((tint != "1") && (tint != "2"))
+            {
+                Console.WriteLine("1 -> anti-glare, 2 -> brown tint");
+                string tintin = Console.ReadLine();
+                tint = tintin;
+            }
+            if (tint == "1")
+            {
+                total += 12.5;
+            }
+            if (tint == "2")
+            {
+                total += 9.99;
+            }
+            if (rx == "1")
+            {
+                total += 40;
+            }
+            if (rx == "2")
+            {
+                total += 25;
+            }
+            Console.WriteLine("Your total cost is " + total.ToString());
+        }
+    }
+}
